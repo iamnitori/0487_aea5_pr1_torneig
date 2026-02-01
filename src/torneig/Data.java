03def0acebc3499a8e3b3aca305fdcd7b48a01f6
jannat402 <bjtvs@gmail.com>
2026-02-01 20:07:32 +0100

    feat: add Data class on torneig

---
+++ src/torneig/Data.java
@@ -0,0 +1,41 @@
+package torneig;
+
+public class Data {
+    private int dia;
+    private int mes;
+    private int any;
+
+    public Data(int dia, int mes, int any) {
+        this.dia = dia;
+        this.mes = mes;
+        this.any = any;
+    }
+
+    // Getters i setters
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAny() {
+		return any;
+	}
+
+	public void setAny(int any) {
+		this.any = any;
+	}
+
+  
+    
+}
