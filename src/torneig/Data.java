cf4f9df23cf923f3435a11f642e83b2077ee99d4
nil <93866229+iamnitori@users.noreply.github.com>
2026-02-03 17:00:29 +0100

    refactor: update formatting and comments in Data class

--- src/torneig/Data.java
+++ src/torneig/Data.java
@@ -1,41 +1,57 @@
 package torneig;
 
+/**
+ * Representa una data amb dia, mes i any.
+ */
 public class Data {
+
+    /** Dia de la data */
     private int dia;
+
+    /** Mes de la data */
     private int mes;
+
+    /** Any de la data */
     private int any;
 
+    /**
+     * Constructor de la classe Data.
+     * @param dia dia
+     * @param mes mes
+     * @param any any
+     */
     public Data(int dia, int mes, int any) {
         this.dia = dia;
         this.mes = mes;
         this.any = any;
     }
 
-    // Getters i setters
-	public int getDia() {
-		return dia;
-	}
+    public int getDia() {
+        return dia;
+    }
 
-	public void setDia(int dia) {
-		this.dia = dia;
-	}
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
 
-	public int getMes() {
-		return mes;
-	}
+    public int getMes() {
+        return mes;
+    }
 
-	public void setMes(int mes) {
-		this.mes = mes;
-	}
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
 
-	public int getAny() {
-		return any;
-	}
+    public int getAny() {
+        return any;
+    }
 
-	public void setAny(int any) {
-		this.any = any;
-	}
+    public void setAny(int any) {
+        this.any = any;
+    }
 
-  
-    
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + any;
+    }
 }
