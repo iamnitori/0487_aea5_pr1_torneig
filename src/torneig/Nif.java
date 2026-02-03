3402e1f8020b435dae517d116cbaafa6d62f349f
nil <93866229+iamnitori@users.noreply.github.com>
2026-02-03 17:01:12 +0100

    refactor: update formatting and comments in Nif class

--- src/torneig/Nif.java
+++ src/torneig/Nif.java
@@ -1,31 +1,44 @@
 package torneig;
 
+/**
+ * Representa un NIF format per DNI i lletra.
+ */
 public class Nif {
+
+    /** Número del DNI */
     private int dni;
+
+    /** Lletra del NIF */
     private char lletra;
 
+    /**
+     * Constructor del NIF.
+     * @param dni número de DNI
+     * @param lletra lletra del NIF
+     */
     public Nif(int dni, char lletra) {
         this.dni = dni;
         this.lletra = lletra;
     }
 
-    public int getDni() { 
-    	return dni; 
-    	}
-    public void setDni(int dni) { 
-    	this.dni = dni; 
-    	}
+    public int getDni() {
+        return dni;
+    }
 
-    public char getLletra() { 
-    	return lletra; 
-    	}
-    public void setLletra(char lletra) { 
-    	this.lletra = lletra; 
-    	}
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public char getLletra() {
+        return lletra;
+    }
+
+    public void setLletra(char lletra) {
+        this.lletra = lletra;
+    }
 
     @Override
     public String toString() {
         return dni + "" + lletra;
     }
 }
-
